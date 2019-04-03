37bef76e7fba6303a0c45ec31ebef99ec86313f2
B.A. Hanlon <herbivore137@gmail.com>
2019-04-03 18:16:18 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -2,17 +2,13 @@ def prime?(num)
   bool = true;
   if num > 2 
     arr = (2...num).to_a
-  
     arr.each do |x|
-
       if num % x == 0 
         bool = false 
         break
       end 
     end
-
     bool
-      
   elsif num == 2 
     true
   else
