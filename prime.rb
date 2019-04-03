3f678cb8cc2735d810c16fbf4564600d5450f769
B.A. Hanlon <herbivore137@gmail.com>
2019-04-03 17:40:26 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,26 @@
-# Add  code here!
+def prime?(num)
+
+  if num > 2 
+   arr = (2...num).to_a
+  
+   bool = true 
+  
+  
+
+   
+    arr.each do |x|
+      
+          num % x == 0 ? bool = false : true
+          b
+    end
+
+  
+  return bool
+  
+  
+  elsif num == 2 
+    true
+  else
+    false
+  end
+end
